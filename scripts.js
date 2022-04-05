efb676366ea37a6ddfa456f8918bab16709ede49
Marat Enaliev <enamarat@gmail.com>
2022-04-05 12:43:17 +0300

    Create functions for showing, hiding, and stopping the timer.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 let programs = [];
+let timerStopped = false; 
+
 
 const createProgram = () => {
   const program_name = document.querySelector("#program_input").value;
@@ -30,7 +32,7 @@ const createProgram = () => {
 }
 
 
-const displayPrograms = (program, excercises) => {
+const displayPrograms = (program, exercises) => {
   const div = document.createElement('DIV');
   div.innerHTML += `
       <div class="user_program">
@@ -44,32 +46,32 @@ const displayPrograms = (program, excercises) => {
               <button class="cancel_action_button">No</button>
           </div>
 
-          <div class="excercise_creation">
-            <button class="add_excercise_button">New Exercise</button>
-            <div class="excercise_parameters">
+          <div class="exercise_creation">
+            <button class="add_exercise_button">New Exercise</button>
+            <div class="exercise_parameters">
                 <h3>Exercise parameters</h3>
-                <input class="excercise_name" maxLength="30" placeholder="name">
-                <input class="excercise_time" type="number" min="0" max="9999" placeholder="duration, seconds">
-                <input class="excercise_sets" type="number" min="0" max="9999" placeholder="number of sets">
+                <input class="exercise_name" maxLength="30" placeholder="name">
+                <input class="exercise_time" type="number" min="0" max="9999" placeholder="duration, seconds">
+                <input class="exercise_sets" type="number" min="0" max="9999" placeholder="number of sets">
                 <input class="rest_time_between" type="number" min="0" max="9999" placeholder="rest time between sets, seconds">
-                <input class="rest_time_after" type="number" min="0" max="9999" placeholder="rest time after the excercise, seconds">
-                <button class="excercise_button">+</button>
+                <input class="rest_time_after" type="number" min="0" max="9999" placeholder="rest time after the exercise, seconds">
+                <button class="exercise_button">+</button>
             </div>
           </div>
 
-          <div class="excercises_list">
-            <table class="excercise_table">
+          <div class="exercises_list">
+            <table class="exercise_table">
               <thead>
                 <tr>
                   <th>Exercise</th>
                   <th>Time, sec</th>
                   <th>Number of sets</th>
                   <th>Rest time between sets, sec</th>
-                  <th>Rest time after excercise, sec</th>
+                  <th>Rest time after exercise, sec</th>
                 </tr>
               </thead>
               <tbody>
-              ${excercises}
+              ${exercises}
               </tbody>
             </table>
           </div>
@@ -81,10 +83,10 @@ const displayPrograms = (program, excercises) => {
 
 
 const showParameters = (event) => {
-  if (event.target.className == "add_excercise_button" && event.target.textContent == "New Exercise") {
+  if (event.target.className == "add_exercise_button" && event.target.textContent == "New Exercise") {
     event.target.parentNode.childNodes[3].style.display = "flex";
     event.target.textContent = "Hide";
-  } else if (event.target.className == "add_excercise_button" && event.target.textContent == "Hide") {
+  } else if (event.target.className == "add_exercise_button" && event.target.textContent == "Hide") {
     event.target.parentNode.childNodes[3].style.display = "none";
     event.target.textContent = "New Exercise";
   }
@@ -92,56 +94,56 @@ const showParameters = (event) => {
 
 
 const createExercise = (event) => {
-  if (event.target.className == "excercise_button") {
+  if (event.target.className == "exercise_button") {
     const program_title = event.target.parentNode.parentNode.parentNode.querySelector(".program_title").textContent.trim();
-    let excercise_id = "";
+    let exercise_id = "";
     document.querySelector("#warning").textContent = "";
 
     // don't accept empty fields
-    if (event.target.parentNode.querySelector(".excercise_name").value.length == 0 ||
-        event.target.parentNode.querySelector(".excercise_time").value.length == 0 ||
-        event.target.parentNode.querySelector(".excercise_sets").value.length == 0 ||
+    if (event.target.parentNode.querySelector(".exercise_name").value.length == 0 ||
+        event.target.parentNode.querySelector(".exercise_time").value.length == 0 ||
+        event.target.parentNode.querySelector(".exercise_sets").value.length == 0 ||
         event.target.parentNode.querySelector(".rest_time_between").value.length == 0 ||
         event.target.parentNode.querySelector(".rest_time_after").value.length == 0) {
           document.querySelector("#warning").textContent = "Fill in all the fields, please!";
           return 0;
     }
 
-    // add the excercise to the program
+    // add the exercise to the program
     for (let i = 0; i < programs.length; i++) {
       if (programs[i].name == program_title) {
-        if (programs[i].excercises == undefined) {
-          programs[i].excercises = [];
+        if (programs[i].exercises == undefined) {
+          programs[i].exercises = [];
         }
-        programs[i].excercises.push({
-          excercise_name: event.target.parentNode.querySelector(".excercise_name").value,
-          excercise_time: event.target.parentNode.querySelector(".excercise_time").value,
-          excercise_sets: event.target.parentNode.querySelector(".excercise_sets").value,
+        programs[i].exercises.push({
+          exercise_name: event.target.parentNode.querySelector(".exercise_name").value,
+          exercise_time: event.target.parentNode.querySelector(".exercise_time").value,
+          exercise_sets: event.target.parentNode.querySelector(".exercise_sets").value,
           rest_time_between: event.target.parentNode.querySelector(".rest_time_between").value,
           rest_time_after: event.target.parentNode.querySelector(".rest_time_after").value,
-          excercise_id: programs[i].excercises.length
+          exercise_id: programs[i].exercises.length
         });
-        excercise_id = programs[i].excercises.length;
+        exercise_id = programs[i].exercises.length;
       }
     }
     localStorage.setItem('programs', JSON.stringify(programs));
 
-    // display the excercise
+    // display the exercise
     const row = document.createElement("TR");
     row.innerHTML = `
-      <td class="excercise_id">${excercise_id}</td>
-      <td>${event.target.parentNode.querySelector(".excercise_name").value}</td>
-      <td>${event.target.parentNode.querySelector(".excercise_time").value}</td>
-      <td>${event.target.parentNode.querySelector(".excercise_sets").value}</td>
+      <td class="exercise_id">${exercise_id}</td>
+      <td>${event.target.parentNode.querySelector(".exercise_name").value}</td>
+      <td>${event.target.parentNode.querySelector(".exercise_time").value}</td>
+      <td>${event.target.parentNode.querySelector(".exercise_sets").value}</td>
       <td>${event.target.parentNode.querySelector(".rest_time_between").value}</td>
-      <td class="last_column">${event.target.parentNode.querySelector(".rest_time_after").value} <button class="delete_excercise_button">x</button></td>
+      <td class="last_column">${event.target.parentNode.querySelector(".rest_time_after").value} <button class="delete_exercise_button">x</button></td>
     `;
-    event.target.parentNode.parentNode.parentNode.querySelector('.excercises_list').querySelector(".excercise_table").appendChild(row);
+    event.target.parentNode.parentNode.parentNode.querySelector('.exercises_list').querySelector(".exercise_table").appendChild(row);
   
     // empty fields
-    event.target.parentNode.querySelector(".excercise_name").value = "";
-    event.target.parentNode.querySelector(".excercise_time").value = "";
-    event.target.parentNode.querySelector(".excercise_sets").value = "";
+    event.target.parentNode.querySelector(".exercise_name").value = "";
+    event.target.parentNode.querySelector(".exercise_time").value = "";
+    event.target.parentNode.querySelector(".exercise_sets").value = "";
     event.target.parentNode.querySelector(".rest_time_between").value = "";
     event.target.parentNode.querySelector(".rest_time_after").value = "";
   }
@@ -161,28 +163,28 @@ const displaySavedPrograms = () => {
     programs = loadedPrograms;
   }
 
-  // display the list of excercises
-  let excercises = ``;
+  // display the list of exercises
+  let exercises = ``;
   programs.map(program => {
     const div = document.createElement('DIV');
     let content = ``;
   
-    if (program.excercises != undefined) {
-      for (let i = 0; i < program.excercises.length; i++) {
+    if (program.exercises != undefined) {
+      for (let i = 0; i < program.exercises.length; i++) {
         content += `
         <tr>
-          <td class="excercise_id">${program.excercises[i].excercise_id}</td>
-          <td>${program.excercises[i].excercise_name}</td>
-          <td>${program.excercises[i].excercise_time}</td>
-          <td>${program.excercises[i].excercise_sets}</td>
-          <td>${program.excercises[i].rest_time_between}</td>
-          <td class="last_column">${program.excercises[i].rest_time_after} <button class="delete_excercise_button">x</button></td>
+          <td class="exercise_id">${program.exercises[i].exercise_id}</td>
+          <td>${program.exercises[i].exercise_name}</td>
+          <td>${program.exercises[i].exercise_time}</td>
+          <td>${program.exercises[i].exercise_sets}</td>
+          <td>${program.exercises[i].rest_time_between}</td>
+          <td class="last_column">${program.exercises[i].rest_time_after} <button class="delete_exercise_button">x</button></td>
         </tr>
         `;
       }
     }
-    excercises = content;
-    displayPrograms(program, excercises);
+    exercises = content;
+    displayPrograms(program, exercises);
   });
 }
 
@@ -209,15 +211,15 @@ const hideDeletionWindow = (event) => {
 }
 
 const deleteExercise = (event) => {
-  if (event.target.className == "delete_excercise_button") {
+  if (event.target.className == "delete_exercise_button") {
     const program_title = event.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector(".program_title").textContent.trim();
-    const excercise_id = event.target.parentNode.parentNode.childNodes[1].textContent.trim();
+    const exercise_id = event.target.parentNode.parentNode.childNodes[1].textContent.trim();
     
     programs.forEach(program => {
       if (program.name == program_title) {
-        program.excercises.forEach(excercise => {
-            if (excercise.excercise_id == excercise_id) {
-              program.excercises = program.excercises.filter(element => element.excercise_id != excercise_id);
+        program.exercises.forEach(exercise => {
+            if (exercise.exercise_id == exercise_id) {
+              program.exercises = program.exercises.filter(element => element.exercise_id != exercise_id);
             }
         });
       }
@@ -230,9 +232,10 @@ const deleteExercise = (event) => {
 // Start timer
 const startTimer = (event) => {
   if (event.target.className == "start_button") {
+    timerStopped = false;
     const program_title = event.target.parentNode.querySelector(".program_title").textContent;
     let data = null;
-    excerciseNumber = 0;
+    exerciseNumber = 0;
   
     for (let i = 0; i < programs.length; i++) {
       if (programs[i].name == program_title) {
@@ -240,38 +243,77 @@ const startTimer = (event) => {
         break;
       }
     }
-    playSound(data, 1, data.excercises.length, 0);
+    playSound(data, 1, data.exercises.length, 0);
   }
 }
 
-//sets, duration, timeBetween, timeAfter
-const playSound = (data, count, excercisesLength, excerciseNumber) => {
+const showTimer = (data, exerciseNumber, set, timeCount) => {
+  document.querySelector("#exercise_name").textContent = `${data.exercises[exerciseNumber].exercise_name}`;
+  document.querySelector("#set_number").textContent = `set: ${set}`;
+  document.querySelector("#counter").textContent = timeCount;
+  document.querySelector("#stop_exercise_button").style.display = "inline-block";
+  document.querySelector("#programs").style.display = "none";
+  document.querySelector("#program_creation_div").style.display = "none";
+}
+
+
+const hideTimer = () => {
+  document.querySelector("#exercise_name").textContent = "";
+  document.querySelector("#set_number").textContent = "";
+  document.querySelector("#counter").textContent = "";
+  document.querySelector("#stop_exercise_button").style.display = "none";
+  document.querySelector("#programs").style.display = "block";
+  document.querySelector("#program_creation_div").style.display = "block";
+}
+
+
+const playSound = (data, count, exercisesLength, exerciseNumber) => {
+  if (timerStopped) {
+    return 0;
+  }
   document.querySelector("#start_signal").play();
-  let timeCount = data.excercises[excerciseNumber].excercise_time;
-  console.log(`duration ${data.excercises[excerciseNumber].excercise_time}`);
+  let timeCount = data.exercises[exerciseNumber].exercise_time;
+  showTimer(data, exerciseNumber, count, timeCount);
  
   const interval = setInterval(()=> {
-    console.log(timeCount);
-    timeCount--;
+    if (!timerStopped) {
+      timeCount--;
+      document.querySelector("#exercise_name").textContent = `${data.exercises[exerciseNumber].exercise_name}`;
+      document.querySelector("#set_number").textContent = `set: ${count}`;
+      document.querySelector("#counter").textContent = timeCount;
+    } else {
+      clearInterval(interval);
+    }
+    
     if (timeCount == 0) {
       document.querySelector("#stop_rest").play();
       clearInterval(interval);
-      if (count < data.excercises[excerciseNumber].excercise_sets) {
-        setTimeout(()=>{
-          playSound(data, count+1, excercisesLength, excerciseNumber);
-        }, 4000+(data.excercises[excerciseNumber].rest_time_between*1000));
-      } else if (count == data.excercises[excerciseNumber].excercise_sets) {
-        console.log("end");
-        if (excerciseNumber < excercisesLength-1) {
-          setTimeout(()=>{
-            playSound(data, 1, excercisesLength, excerciseNumber+1);
-          }, 4000+(data.excercises[excerciseNumber].rest_time_after*1000));
-        } else if (excerciseNumber == excercisesLength-1) {
-          setTimeout(()=>document.querySelector("#mission_complete").play(), 4000);
+      if (count < data.exercises[exerciseNumber].exercise_sets) {
+        setTimeout(()=> playSound(data, count+1, exercisesLength, exerciseNumber), 4000+(data.exercises[exerciseNumber].rest_time_between*1000));
+      } else if (count == data.exercises[exerciseNumber].exercise_sets) {
+        if (exerciseNumber < exercisesLength-1) {
+          setTimeout(()=> playSound(data, 1, exercisesLength, exerciseNumber+1), 4000+(data.exercises[exerciseNumber].rest_time_after*1000));
+        } else if (exerciseNumber == exercisesLength-1) {
+          setTimeout(()=> {
+            hideTimer();
+            document.querySelector("#mission_complete").play()
+          }, 4000);
         }
       }
     }
   }, 1000);
+}
+
+
+const stopTimer = () => {
+  document.querySelector("#exercise_name").textContent = "";
+  document.querySelector("#set_number").textContent = "";
+  document.querySelector("#counter").textContent = "";
+  document.querySelector("#stop_exercise_button").style.display = "none";
+  document.querySelector("#programs").style.display = "block";
+  document.querySelector("#program_creation_div").style.display = "block";
+  timerStopped = true;
+  hideTimer();
 }
 
 
@@ -284,3 +326,4 @@ document.querySelector("#programs").addEventListener("click", deleteProgram);
 document.querySelector("#programs").addEventListener("click", deleteExercise);
 document.querySelector("#programs").addEventListener("click", startTimer);
 window.addEventListener("load", displaySavedPrograms);
+document.querySelector("#stop_exercise_button").addEventListener("click", stopTimer);
